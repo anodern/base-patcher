04e2f07e8a2b5d59f11739604fdd5e97219539d0
anodern <jaznyang@hotmail.com>
2020-09-09 16:14:48 +0800

    Initial commit

---
+++ base安装器/Form1.Designer.cs
@@ -0,0 +1,149 @@
+﻿namespace base安装器 {
+	partial class Form1 {
+		/// <summary>
+		/// 必需的设计器变量。
+		/// </summary>
+		private System.ComponentModel.IContainer components = null;
+
+		/// <summary>
+		/// 清理所有正在使用的资源。
+		/// </summary>
+		/// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+		protected override void Dispose(bool disposing) {
+			if (disposing && (components != null)) {
+				components.Dispose();
+			}
+			base.Dispose(disposing);
+		}
+
+		#region Windows 窗体设计器生成的代码
+
+		/// <summary>
+		/// 设计器支持所需的方法 - 不要修改
+		/// 使用代码编辑器修改此方法的内容。
+		/// </summary>
+		private void InitializeComponent() {
+            this.components = new System.ComponentModel.Container();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.label1 = new System.Windows.Forms.Label();
+            this.timer2 = new System.Windows.Forms.Timer(this.components);
+            this.timer3 = new System.Windows.Forms.Timer(this.components);
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.panel1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // timer1
+            // 
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // listBox1
+            // 
+            this.listBox1.Font = new System.Drawing.Font("微软雅黑", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.ItemHeight = 17;
+            this.listBox1.Location = new System.Drawing.Point(12, 13);
+            this.listBox1.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(166, 208);
+            this.listBox1.TabIndex = 0;
+            this.listBox1.SelectedIndexChanged += new System.EventHandler(this.listBox1_SelectedIndexChanged);
+            // 
+            // button1
+            // 
+            this.button1.Enabled = false;
+            this.button1.Font = new System.Drawing.Font("微软雅黑", 10.5F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.button1.Location = new System.Drawing.Point(204, 175);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(173, 46);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "安装";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(255)))), ((int)(((byte)(192)))));
+            this.panel1.Controls.Add(this.label1);
+            this.panel1.Location = new System.Drawing.Point(372, 12);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(428, 280);
+            this.panel1.TabIndex = 2;
+            this.panel1.Visible = false;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("微软雅黑 Light", 24F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.label1.Location = new System.Drawing.Point(37, 92);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(322, 41);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "正在安装，请稍后......";
+            // 
+            // timer2
+            // 
+            this.timer2.Tick += new System.EventHandler(this.timer2_Tick);
+            // 
+            // timer3
+            // 
+            this.timer3.Tick += new System.EventHandler(this.timer3_Tick);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("微软雅黑 Light", 24F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.label2.Location = new System.Drawing.Point(197, 13);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(180, 41);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "base安装器";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("微软雅黑 Light", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.label3.Location = new System.Drawing.Point(207, 86);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(170, 42);
+            this.label3.TabIndex = 4;
+            this.label3.Text = "在左侧列表中选择相应\r\n安装包并点击安装按钮";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 17F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(392, 258);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.listBox1);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label3);
+            this.Font = new System.Drawing.Font("微软雅黑", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.Name = "Form1";
+            this.Text = "base安装器";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+		}
+
+		#endregion
+		private System.Windows.Forms.Timer timer1;
+		private System.Windows.Forms.ListBox listBox1;
+		private System.Windows.Forms.Button button1;
+		private System.Windows.Forms.Panel panel1;
+		private System.Windows.Forms.Label label1;
+		private System.Windows.Forms.Timer timer2;
+		private System.Windows.Forms.Timer timer3;
+		private System.Windows.Forms.Label label2;
+		private System.Windows.Forms.Label label3;
+	}
+}
+
---
+++ base安装器/Form1.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Diagnostics;
+using System.Drawing;
+using System.IO;
+using System.Windows.Forms;
+
+namespace base安装器 {
+	public partial class Form1 : Form {
+		public Form1() {
+			InitializeComponent();
+		}
+
+		private void Form1_Load(object sender, EventArgs e) {
+			GetExtensionFile("tar");
+		}
+
+		private void GetExtensionFile(string extension) {
+			DirectoryInfo theFolder = new DirectoryInfo(Environment.CurrentDirectory);
+			FileInfo[] fileInfo = theFolder.GetFiles();
+			foreach (FileInfo NextFile in fileInfo) {
+				if (NextFile.Extension == "."+ extension) {
+					listBox1.Items.Add(NextFile.Name);
+				}
+			}
+		}
+
+		private void listBox1_SelectedIndexChanged(object sender, EventArgs e) {
+			button1.Text = "安装" + listBox1.SelectedItem.ToString().Substring(0,listBox1.SelectedItem.ToString().Length-4);
+			button1.Enabled = true;
+		}
+		
+		Process process = new Process();
+		private void button1_Click(object sender, EventArgs e) {
+			process.StartInfo.FileName = Environment.CurrentDirectory+@"\nbs.exe";
+			process.StartInfo.Arguments = @" x "+ @" -o.\temp " + listBox1.SelectedItem;
+			process.StartInfo.WindowStyle = ProcessWindowStyle.Hidden;
+			process.Start();
+			timer1.Enabled = true;
+			panel1.Location=new Point(0,0);
+			panel1.Visible = true;
+		}
+		
+		private void timer1_Tick(object sender, EventArgs e) {
+			if (process.HasExited) {
+				panel1.Visible = false;
+				timer2.Enabled = true;
+				timer1.Enabled = false;
+			}
+		}
+
+		private void timer2_Tick(object sender, EventArgs e) {
+			process.StartInfo.Arguments = @" a base.scs .\temp\* -tzip";
+			process.Start();
+			timer3.Enabled = true;
+			panel1.Location = new Point(0, 0);
+			panel1.Visible = true;
+			timer2.Enabled = false;
+		}
+
+		private void timer3_Tick(object sender, EventArgs e) {
+			if (process.HasExited) {
+				panel1.Visible = false;
+				Directory.Delete(Environment.CurrentDirectory + @"\temp", true);
+				timer3.Enabled = false;
+			}
+		}
+	}
+}
